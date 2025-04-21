856af6c98e7a2b38724f7acd890b8d153e1c81ce
Jim Kettleson <jjkettleson@yahoo.com>
2025-04-21 18:08:04 -0500

    Cart Items Task 3

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  items: [],
+};
+
 export const CartSlice = createSlice({
   name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
+  initialState,
+    reducers: {
+      addItem: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity += 1;
+        } else {
+          state.items.push({ name, image, cost , quantity: 1 });
+        }
+      },
     
+      removeItem: (state, action) => {
+        const itemName = action.payload;
+        state.items = state.items.filter((item) => item.name !== itemName);
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
-    },
+    updateQuantity: (state, action) => {
+    const { name, amount } = action.payload;
+    const item = state.items.find((item) => item.name === name);
+    if (item) {
+        item.quantity = amount;
+    }
+  },
   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
